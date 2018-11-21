087f68afc75c1858f4c95bcad4da27a262ac0bb8
youngju <youngjucha0316@gmail.com>
2018-11-21 17:27:03 +0900

    동우등록

--- app/src/main/java/com/devstories/aninuriandroid/activities/UseActivity.kt
+++ app/src/main/java/com/devstories/aninuriandroid/activities/UseActivity.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.view.View
 import com.devstories.aninuriandroid.R
 import kotlinx.android.synthetic.main.activity_coupon_use.*
 
@@ -24,20 +25,24 @@ class UseActivity :  FragmentActivity() {
 
         useLL.setBackgroundResource(R.drawable.background_strock_707070)
         supportFragmentManager.beginTransaction().replace(R.id.main_frame, UseFragment).commit()
+        use_op_LL.visibility = View.VISIBLE
 
 
 
-        phonET.setOnClickListener {
-
-        }
         useLL.setOnClickListener {
             setmenu()
+            phonET.setHint("휴대전화번호를 눌러주세요")
+            titleTV.text = "사용/조회"
+            use_op_LL.visibility = View.VISIBLE
             useLL.setBackgroundResource(R.drawable.background_strock_707070)
             supportFragmentManager.beginTransaction().replace(R.id.main_frame, UseFragment).commit()
         }
 
         couponLL.setOnClickListener {
             setmenu()
+            phonET.setHint("사용할 포인트를 입력하세요.")
+            titleTV.text = "쿠폰/포인트\n조회"
+            use_op_LL.visibility = View.GONE
             couponLL.setBackgroundResource(R.drawable.background_strock_707070)
             supportFragmentManager.beginTransaction().replace(R.id.main_frame, CouponFragment).commit()
         }
--- app/src/main/java/com/devstories/aninuriandroid/activities/UserListActivity.kt
+++ app/src/main/java/com/devstories/aninuriandroid/activities/UserListActivity.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
+import android.view.View
 import com.devstories.aninuriandroid.R
 import com.devstories.aninuriandroid.adapter.UserVisitAdapter
 import kotlinx.android.synthetic.main.activity_user_list.*
@@ -28,36 +29,37 @@ class UserListActivity : FragmentActivity() {
         val Message_Manage_Fragment : Message_Manage_Fragment = Message_Manage_Fragment()
         val Point_List_Fragment : Point_List_Fragment = Point_List_Fragment()
 
-
         userLL.setBackgroundResource(R.drawable.background_strock_707070)
+        useroptionLL.visibility = View.VISIBLE
         supportFragmentManager.beginTransaction().replace(R.id.userFL, User_List_Fragment).commit()
-
-
 
         userLL.setOnClickListener {
             setmenu()
             titleTV.text = "고객목록"
+            useroptionLL.visibility = View.VISIBLE
             userLL.setBackgroundResource(R.drawable.background_strock_707070)
             supportFragmentManager.beginTransaction().replace(R.id.userFL, User_List_Fragment).commit()
         }
-
         pointLL.setOnClickListener {
 
             setmenu()
             titleTV.text = "포인트 내역"
             pointLL.setBackgroundResource(R.drawable.background_strock_707070)
+            point_op_LL.visibility = View.VISIBLE
             supportFragmentManager.beginTransaction().replace(R.id.userFL, Point_List_Fragment).commit()
         }
         uservisitLL.setOnClickListener {
             setmenu()
-            titleTV.text = "고객 방문 분석"
+            titleTV.text = "고객 방문\n분석"
             uservisitLL.setBackgroundResource(R.drawable.background_strock_707070)
+            uservisit_op_LL.visibility = View.VISIBLE
             supportFragmentManager.beginTransaction().replace(R.id.userFL, User_visit_List_Fragment).commit()
         }
         messageLL.setOnClickListener {
             setmenu()
-            titleTV.text = "메세지 쿠폰 관리"
+            titleTV.text = "메세지/\n쿠폰 관리"
             messageLL.setBackgroundResource(R.drawable.background_strock_707070)
+            message_op_LL.visibility = View.VISIBLE
             supportFragmentManager.beginTransaction().replace(R.id.userFL, Message_Manage_Fragment).commit()
         }
 
@@ -69,6 +71,10 @@ class UserListActivity : FragmentActivity() {
         userLL.setBackgroundResource(R.drawable.background_strock_null)
         messageLL.setBackgroundResource(R.drawable.background_strock_null)
         uservisitLL.setBackgroundResource(R.drawable.background_strock_null)
+        useroptionLL.visibility = View.GONE
+        message_op_LL.visibility = View.GONE
+        point_op_LL.visibility = View.GONE
+        uservisit_op_LL.visibility = View.GONE
     }
 
 
