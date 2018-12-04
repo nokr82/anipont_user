e69aa1d897839e39d335fa1666e791eca0ecdbcf
admin <chashin22>
2018-12-04 12:30:43 +0900

    신행 올림

--- app/src/main/java/com/devstories/aninuriandroid/adapter/CouponListAdapter.kt
+++ app/src/main/java/com/devstories/aninuriandroid/adapter/CouponListAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.devstories.aninuriandroid.R
 import com.devstories.aninuriandroid.base.Utils
@@ -45,13 +46,45 @@ open class CouponListAdapter (context: Context, view:Int, data: ArrayList<JSONOb
         if (used == "N" && del == "N") {
 
             val coupon_name = Utils.getString(coupon, "name")
-            val coupon_type = Utils.getString(coupon, "type")
-            val coupon_valid = SimpleDateFormat("yyyy-MM-dd")
+            val coupon_type = Utils.getInt(coupon, "type")
+            val coupon_s_valid = SimpleDateFormat("yyyy-MM-dd")
+                    .parse(Utils.getString(memberCoupon, "s_use_date"))
+            val coupon_e_valid = SimpleDateFormat("yyyy-MM-dd")
                     .parse(Utils.getString(memberCoupon, "e_use_date"))
-            val coupon_week = Utils.getString(coupon, "week_use_yn")
-            val coupon_sat = Utils.getString(coupon, "sat_use_yn")
-            val coupon_sun = Utils.getString(coupon, "sun_use_yn")
+            val coupon_message = Utils.getString(coupon, "message")
 
+            var backgroundImg = R.mipmap.coupon
+
+            when(coupon_type) {
+                1 -> {
+                    backgroundImg = R.mipmap.coupon_first
+                }
+                2 -> {
+                    backgroundImg = R.mipmap.coupon_second
+                }
+                3 -> {
+                    backgroundImg = R.mipmap.coupon_third
+                }
+                4 -> {
+                    backgroundImg = R.mipmap.coupon_fourth
+                }
+                5 -> {
+                    backgroundImg = R.mipmap.coupon_first
+                }
+                6 -> {
+                    backgroundImg = R.mipmap.coupon_second
+                }
+            }
+
+            item.couponBackLL.setBackgroundResource(backgroundImg)
+            item.item_couponNameTV.text = coupon_name
+            item.item_messageTV.text = coupon_message
+            item.item_s_validityTV.text = coupon_s_valid.toString()
+            item.item_e_validityTV.text = coupon_e_valid.toString()
+
+            /*val coupon_week = Utils.getString(coupon, "week_use_yn")
+            val coupon_sat = Utils.getString(coupon, "sat_use_yn")
+            val coupon_sun = Utils.getString(coupon, "sun_use_yn")*/
 
         }
 
@@ -71,23 +104,19 @@ open class CouponListAdapter (context: Context, view:Int, data: ArrayList<JSONOb
     }
 
     class ViewHolder(v : View) {
+        var couponBackLL :LinearLayout
         var item_couponNameTV :TextView
-        var item_couponTypeTV :TextView
+        var item_s_validityTV :TextView
+        var item_e_validityTV :TextView
         var item_messageTV :TextView
-        var item_weekTV :TextView
-        var item_satTV :TextView
-        var item_sunTV :TextView
-        var item_validityTV :TextView
 
 
         init {
+            couponBackLL = v.findViewById(R.id.couponBackLL)
             item_couponNameTV = v.findViewById(R.id.item_couponNameTV)
-            item_couponTypeTV = v.findViewById(R.id.item_couponTypeTV)
+            item_s_validityTV = v.findViewById(R.id.item_s_validityTV)
+            item_e_validityTV = v.findViewById(R.id.item_e_validityTV)
             item_messageTV = v.findViewById(R.id.item_messageTV)
-            item_weekTV = v.findViewById(R.id.item_weekTV)
-            item_satTV = v.findViewById(R.id.item_satTV)
-            item_sunTV = v.findViewById(R.id.item_sunTV)
-            item_validityTV = v.findViewById(R.id.item_validityTV)
         }
     }
 }
